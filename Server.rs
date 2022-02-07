c5c64ad4c45fb2bda753ef08af7d7eecff35a8c1
Sp0wn <CocoRS@protonmail.com>
2022-02-07 16:13:00 -0600

    Basic connection

--- Server.rs
+++ Server.rs
@@ -1,22 +1,9 @@
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:12321").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(move || handle_client(stream));
-    }
-}
-
-fn handle_client(stream: TcpStream) {
-    let mut stream = BufReader::new(stream);
-    loop {
-        let mut buf = String::new();
-        if stream.read_line(&mut buf).is_err() {
-            break;
-        }
-        stream.get_ref().write(buf.as_bytes()).unwrap();
     }
 }

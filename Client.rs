89a90c55779190b895f092d630832cdb3077fdf9
Sp0wn <CocoRS@protonmail.com>
2022-02-07 17:56:49 -0600

    Basic threads and messaging

--- Client.rs
+++ Client.rs
@@ -1,5 +1,13 @@
 use std::net::TcpStream;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:9000").unwrap();
+    let stream = TcpStream::connect("127.0.0.1:9000").unwrap();
+
+    let mut reader = BufReader::new(stream);
+    let mut buffer = String::new();
+
+    reader.read_line(&mut buffer).unwrap();
+    print!("{}", buffer);
 }
--- Server.rs
+++ Server.rs
@@ -1,9 +1,18 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+
+        thread::spawn(move || handle_connection(stream));
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let message = String::from("Message\n");
+    stream.write(message.as_bytes()).unwrap();
 }
